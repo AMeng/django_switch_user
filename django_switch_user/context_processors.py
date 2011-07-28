873f04d41b15477a464420454b30c6912bc42929
alex <alexbmeng@gmail.com>
2011-07-28 17:46:49 -0400

    Changed the context processor into a class based view

--- django_switch_user/context_processors.py
+++ django_switch_user/context_processors.py
@@ -1,20 +1,39 @@
-from django import forms
-from django.contrib.auth.models import User
-from django.conf import settings
-
-def switch_user(request):
+class switch_user(object):
 	"""
-	Create a form and pass it into the context, for super users.
-
-	This form is a single field, where a super user can change
-	to another user to see how the page looks for them.
+	This is a class based context processor. This provides a
+	form for super users to switch to another user.
 	"""
 
-	if request.user.is_superuser or request.user.has_perm('Switch User') or \
-		(request.session and request.session.has_key('superuser-switch')):
+	def __new__(cls, request,*args, **kwargs):
+		obj = super(switch_user,cls).__new__(cls)
+		return obj(request, *args, **kwargs)
 
+	def __call__(self,request,*args,**kwargs):
+		if self.is_allowed_to_switch(request):
+			form = self.get_form()
+
+			return {self.get_context_var_name(): form(prefix=self.get_form_prefix()) }
+		return {}
+
+	def is_allowed_to_switch(self,request):
+		if request.user.is_superuser or \
+			request.user.has_perm('Switch User') or \
+			(request.session and request.session.has_key('superuser-switch')):
+			return True
+		return False
+
+	def get_user_queryset(self):
+		from django.contrib.auth.models import User
+		return User.objects.filter(is_active=True).order_by('username')
+
+	def get_form(self):
+		from django import forms
 		class SelectUser(forms.Form):
-			user = forms.ModelChoiceField(queryset=User.objects.all())
+			user = forms.ModelChoiceField(queryset=self.get_user_queryset())
+		return SelectUser
 
-		return {'switch_user_form': SelectUser(prefix="django-switch") }
-	return {}
+	def get_context_var_name(self):
+		return "switch_user_form"
+
+	def get_form_prefix(self):
+		return "django-switch"
