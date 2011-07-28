fd249faeaf916986696ac200d786c79df2794106
alex <alexbmeng@gmail.com>
2011-07-28 17:47:13 -0400

    Implemented the custom backend in the middleware

--- django_switch_user/middleware.py
+++ django_switch_user/middleware.py
@@ -1,31 +1,38 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
-class SwitchUser:
+class SwitchUser():
 	def process_request(self,request):
+
+		"""
+		If the switch user form has been submitted, validate that the user
+		is either a superuser, or they have the correct session flag
+		(which is set below when needed). We use a custom auth backend to
+		'validate' the user
+		"""
+
 		if (request.user.is_superuser or request.session.has_key('superuser-switch')) and \
 			request.POST and 'django-switch-user' in request.POST:
-				
+			
+			new_username = User.objects.get(id=request.POST['django-switch-user']).username
+
+			if request.session.has_key('superuser-switch'):
+				new_user = authenticate(original_username=request.user.username, new_username=new_username, superuser_session=True)
+			else:
+				new_user = authenticate(original_username=request.user.username, new_username=new_username)
+			
+			login(request,new_user)
+
+			request.user = new_user
+			request.session['_auth_user_id'] = new_user.id
+
 			"""
 			We put a flag variable on the session, so we know this session belongs 
 			to a super user. This way, the context processor knows to display the
 			form to switch users again.
 			"""
+
 			request.session['superuser-switch'] = True
 
-			# Switch the user
-			user = User.objects.get(id=request.POST['django-switch-user'])
-			request.session['_auth_user_id'] = user.id
-			request.user = user
-
-			"""
-			Here, we clear out request.POST. We want the regular view to run, but
-			not with a (mostly) empty request.POST. This avoids problems where
-			the view checks "if request.POST" and then does something special.
-			This only happens during the actual user switch process. It does not
-			effect POSTing as the new user later on.
-			"""
-
-			request.POST._mutable = True
-			request.POST.clear()
-
 		return
+
