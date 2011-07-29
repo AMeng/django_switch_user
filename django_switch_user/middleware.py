12fd7b26abdb4c5e7d037d7d700d398e6356fef9
alex <alexbmeng@gmail.com>
2011-07-29 12:32:44 -0400

    Moved the work of the context processor into the middleware's process_response method

--- django_switch_user/middleware.py
+++ django_switch_user/middleware.py
@@ -1,5 +1,22 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from django import forms
+from django.template.loader import render_to_string
+from django.utils.encoding import smart_unicode
+from django.views.decorators.csrf import csrf_exempt
+
+def replace_insensitive(string, target, replacement):
+	"""
+	Similar to string.replace() but is case insensitive
+	Taken from Django Debug Toolbar
+	"""
+	no_case = string.lower()
+	index = no_case.rfind(target.lower())
+	if index >= 0:
+		return string[:index] + replacement + string[index + len(target):]
+	else: # no results so return the original string
+		return string
 
 class SwitchUser():
 	def process_request(self,request):
@@ -28,11 +45,65 @@ class SwitchUser():
 
 			"""
 			We put a flag variable on the session, so we know this session belongs 
-			to a super user. This way, the context processor knows to display the
-			form to switch users again.
+			to a super user. This way, process_response knows to display the form
+			to switch users again.
 			"""
 
 			request.session['superuser-switch'] = True
 
 		return
 
+	def process_response(self,request,response):
+		"""
+		Embed the form template into the response content
+		"""
+
+		context = {}
+
+		if request.user.is_superuser or \
+			request.user.has_perm('Switch User') or \
+			(request.session and request.session.has_key('superuser-switch')):
+
+			form = self.get_form()
+
+			context = {self.get_context_var_name(): form(prefix=self.get_form_prefix()) }
+
+			if 'csrftoken' in request.COOKIES:
+				context['csrf_token'] = request.COOKIES['csrftoken']
+
+		response.content = replace_insensitive(
+			smart_unicode(response.content),
+			u'</body>',
+			smart_unicode(render_to_string(self.get_template(),context) + u'</body>')
+		)
+
+		if response.get('Content-Length', None):
+			response['Content-Length'] = len(response.content)
+
+		return response
+
+	def get_user_queryset(self):
+		if hasattr(settings, 'DJANGO_SWITCH_USER_QUERYSET'):
+			return settings.DJANGO_SWITCH_USER_QUERYSET(User.objects.all())
+
+		return User.objects.filter(is_active=True).order_by('username')
+
+	def get_form(self):
+		class SelectUser(forms.Form):
+			user = forms.ModelChoiceField(queryset=self.get_user_queryset())
+			user.label_from_instance = self.get_user_label
+		return SelectUser
+
+	def get_user_label(self,user):
+		if hasattr(settings, 'DJANGO_SWITCH_USER_LABEL'):
+			return settings.DJANGO_SWITCH_USER_LABEL(user)
+		return "%s" % user
+
+	def get_context_var_name(self):
+		return "switch_user_form"
+
+	def get_form_prefix(self):
+		return "django-switch"
+
+	def get_template(self):
+		return "django_switch_user/switch_user_form.html"
